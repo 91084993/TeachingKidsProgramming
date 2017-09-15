db3ce510034a05e53465d12d162bdbf9f7378fd8
91084993 <31664271+91084993@users.noreply.github.com>
2017-09-15 13:32:50 -0700

    added copies of Houses.java and started FourSquare.java

--- src/org/teachingkidsprogramming/section02methods/Variations/Houses_Pointy.java
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses_Pointy.java
@@ -2,29 +2,35 @@ package org.teachingkidsprogramming.section02methods.Variations;
 
 import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors.Reds;
-import org.teachingextensions.virtualproctor.VirtualProctor;
 
-public class Houses3
+public class Houses_Pointy
 {
   public static void main(String[] args)
   {
-    VirtualProctor.setClassName("Palm Digital");
-    VirtualProctor.setClassName("Nathan R");
     Tortoise.show();
     Tortoise.setSpeed(10);
     Tortoise.setX(200);
     int height = 40;
-    drawHouse(height);
-    drawHouse(120);
-    drawHouse(90);
-    drawHouse(20);
+    drawHouseFlatRoof(height);
+    drawHousePointyRoof(120);
+    //drawHouse(90);
+    //drawHouse(20);
   }
-  private static void drawHouse(int height)
+  private static void drawHousePointyRoof(int height)
   {
     Tortoise.setPenColor(Reds.Red);
     Tortoise.move(height);
-    //drawFlatRoof();
     drawPointyRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawHouseFlatRoof(int height)
+  {
+    Tortoise.setPenColor(Reds.Red);
+    Tortoise.move(height);
+    drawFlatRoof();
     Tortoise.move(height);
     Tortoise.turn(-90);
     Tortoise.move(20);
@@ -37,12 +43,11 @@ public class Houses3
     Tortoise.turn(90);
     Tortoise.move(15);
     Tortoise.turn(45);
-    Tortoise.move(15);
   }
   private static void drawFlatRoof()
   {
-    Tortoise.turn(-90);
-    Tortoise.move(20);
-    Tortoise.turn(-90);
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
   }
 }
