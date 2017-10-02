2db5e6bd00a1f164fcfde9dfda915f765b37079c
91084993 <31664271+91084993@users.noreply.github.com>
2017-10-02 10:45:06 -0700

    made game ask to guess between 1 and 100

---
+++ src/org/teachingkidsprogramming/section03ifs/HiLow4.java
@@ -0,0 +1,50 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import java.util.Random;
+
+import org.teachingextensions.logo.Sound;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+//this variation tells the user how many guesses they have left
+
+public class HiLow4
+{
+  public static void main(String[] args)
+  {
+    Random rand = new Random();
+    int answer = rand.nextInt(100) + 1;
+    // int answer = (int) (Math.random() * 100 + 1);
+    //                                max^ min^
+    //System.out.println("The answer is" + answer);
+    //int answer = 41;
+    int upperRange = 100;
+    for (int i = 1; i <= 8; i++)
+    {
+      MessageBox.showMessage("Please enter a guess between 1 and 100");
+      int guess = MessageBox.askForNumericalInput("What is your guess?");
+      //input validation
+      while (guess < 1 || guess > upperRange)
+      {
+        MessageBox.showMessage("Please enter a valid guess between 1 and " + "" + upperRange);
+        guess = MessageBox.askForNumericalInput("What is your guess?");
+      }
+      if (guess == answer)
+      {
+        Sound.playBeep();
+        MessageBox.showMessage("You Won!!!");
+        break;
+      }
+      else if (guess > answer)
+      {
+        MessageBox.showMessage("Too high");
+      }
+      else
+      {
+        MessageBox.showMessage("Too low");
+      }
+      if (i == 8)
+      {
+        MessageBox.showMessage("You lost");
+      }
+    }
+  }
+}
