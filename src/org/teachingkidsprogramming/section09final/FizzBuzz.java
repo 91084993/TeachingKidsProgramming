861f7cb2b58bc2656d1ea12cefe1c351289259f3
91084993 <31664271+91084993@users.noreply.github.com>
2017-12-06 08:22:55 -0800

    fixed some errors in FizzBuzz

--- src/org/teachingkidsprogramming/section09final/FizzBuzz.java
+++ src/org/teachingkidsprogramming/section09final/FizzBuzz.java
@@ -1,23 +1,21 @@
 package org.teachingkidsprogramming.section09final;
 
-import javax.swing.text.StyledEditorKit.ForegroundAction;
-
-import org.teachingkidsprogramming.recipes.inDevelopment.forLoopUpdate;
-
 public class FizzBuzz
 {
-  //   For the whole numbers from 1 to 100, print either that number, or, 
-  for ( int i = 1; i < 100; i++)
   {
-    System.out.print(i);
+    //   For the whole numbers from 1 to 100, print either that number, or, 
+    for (int i = 1; i < 100; i++)
+    {
+      System.out.print(i);
+    }
+    //    If that number is divisible by 3, then print the word 'Fizz', 
+    //    If that number is divisible by 5, then print the word 'Buzz', 
+    //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
+    //
+    //    NOTE: this is a kata (higher level instructions)
+    //    part of the exercise is to translate into line-by-line English, THEN Java
+    //
+    //    For more complete directions see this page
+    //    https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
   }
-  //    If that number is divisible by 3, then print the word 'Fizz', 
-  //    If that number is divisible by 5, then print the word 'Buzz', 
-  //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
-  //
-  //    NOTE: this is a kata (higher level instructions)
-  //    part of the exercise is to translate into line-by-line English, THEN Java
-  //
-  //    For more complete directions see this page
-  //    https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
 }
